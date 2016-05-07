00da2467cde8a667ededfc74046db4f651e8de65
jesseN <jessemezini@gmail.com>
2016-05-07 18:26:49 -0300

    completed

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,7 +2,9 @@ import axios from 'axios';
 
 import {
   FETCH_POSTS,
-  CREATE_POST
+  FETCH_POST,
+  CREATE_POST,
+  DELETE_POST
 } from './action-types';
 
 const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
@@ -22,6 +24,24 @@ export function createPost(props) {
 
   return {
     type: CREATE_POST,
+    payload: request
+  };
+}
+
+export function fetchPost(id) {
+  const request = axios.get(`${ROOT_URL}/posts/${id}${API_KEY}`);
+
+  return {
+    type: FETCH_POST,
+    payload: request
+  };
+}
+
+export function deletePost(id) {
+  const request = axios.delete(`${ROOT_URL}/posts/${id}${API_KEY}`);
+
+  return {
+    type: DELETE_POST,
     payload: request
   };
 }
--- src/components/ShowPost.js
+++ src/components/ShowPost.js
@@ -1,13 +1,54 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
+import { connect } from 'react-redux';
+import { fetchPost, deletePost } from '../actions';
+import { Link } from 'react-router';
 
 class ShowPost extends Component {
+  constructor() {
+    super();
+
+    this.onDeletePost = this.onDeletePost.bind(this);
+  }
+
+  componentWillMount() {
+    this.props.fetchPost(this.props.params.id);
+  }
+
+  onDeletePost() {
+    this.props.deletePost(this.props.params.id);
+  }
+
+  onDeletePostSubmit(props) {
+    this.props.deletePost(props)
+      .then(() => {
+        this.context.router.push('/');
+      });
+  }
+
   render() {
+    const { post } = this.props;
+
+    if (!post) {
+      return <div>Loading...</div>
+    }
     return (
       <div>
-        Show post {this.props.params.id}
+        <Link to="/">Back to posts list.</Link>
+        <button className="btn btn-danger pull-xs-right" onClick={this.onDeletePost}>Detele post</button>
+        <h3>{post.title}</h3>
+        <h6>Categories: {post.categories}</h6>
+        <p>{post.content}</p>
       </div>
     );
   }
 }
 
-export default ShowPost;
+ShowPost.contextTypes = {
+  router: PropTypes.object
+};
+
+function mapStateToProps(state) {
+  return { post: state.posts.post };
+}
+
+export default connect(mapStateToProps, { fetchPost, deletePost })(ShowPost);
